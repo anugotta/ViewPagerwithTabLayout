265afadee685d121fec47a08678b41b87ffad04b
ASP <anugotta@gmail.com>
2017-04-22 02:55:58 +0530

    Initial Commit

---
+++ app/src/main/java/com/gadgetsaint/viewpagerexample/MainActivity.java
@@ -0,0 +1,69 @@
+package com.gadgetsaint.viewpagerexample;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gadgetsaint.viewpagerexample.fragments.FragmentOne;
+import com.gadgetsaint.viewpagerexample.fragments.FragmentThree;
+import com.gadgetsaint.viewpagerexample.fragments.FragmentTwo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new FragmentOne(), "FRAG1");
+        adapter.addFragment(new FragmentTwo(), "FRAG2");
+        adapter.addFragment(new FragmentThree(), "FRAG3");
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+    }
+
+
+
+// Adapter for the viewpager using FragmentPagerAdapter
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+}
---
+++ app/src/main/java/com/gadgetsaint/viewpagerexample/fragments/FragmentOne.java
@@ -0,0 +1,35 @@
+package com.gadgetsaint.viewpagerexample.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gadgetsaint.viewpagerexample.R;
+
+/**
+ * Created by Anu on 22/04/17.
+ */
+
+
+
+public class FragmentOne extends Fragment {
+
+    public FragmentOne() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+
+}
